73ecb450957cab604a4df8de04c951b14f4246ec
Rod Bromberg <brombergrod@gmail.com>
2023-01-07 18:44:15 -0500

    initial client app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App bg-red-900">
+      hellloo
+    </div>
+  )
+}
+
+export default App

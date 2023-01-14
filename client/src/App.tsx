809dbd2e96ec74923effb30f747ab8888b839f76
Rod Bromberg <brombergrod@gmail.com>
2023-01-14 13:55:35 -0500

    codemiodemirror set up and added server folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
+// import "codemirror/keymap/sublime"
+// import "codemirror/theme/dracula.css"
+import CodeMirror, {useCodeMirror} from '@uiw/react-codemirror'
+
 import './App.css'
 
+import { javascript } from '@codemirror/lang-javascript';
+
 function App() {
+  const [code,setCode] = useState('')
+  const handleSubmit = () => {
+    console.log(code)
+  }
+  const onChange = useCallback((value: string, viewUpdate: any) => {
+    setCode(value)
+    console.log('value:', value);
+  }, []);
   return (
-    <div className="App bg-red-900">
-      hellloo
+    <div>
+    <CodeMirror
+      value={code}
+      height="200px"
+      extensions={[javascript()]}
+      onChange={onChange}
+      theme={'dark'}
+      key='sublime'
+    />
+    <div onClick={handleSubmit} className="border-2 bg-blue-600">Submit</div>
     </div>
-  )
+  );
 }
 
 export default App
